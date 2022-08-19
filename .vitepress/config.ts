845233d6c185e5eb5797ccb3bd00d801dbd744f5
jimmy <s10219505@connect.np.edu.sg>
2022-08-19 15:31:12 +0800

    feat: init sidebar

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -27,6 +27,49 @@ export default defineConfig({
     ]
   },
   themeConfig: {
+    logo: 'assets/logo.svg',
     siteTitle: 'Overflow Handbook',
+    nav: [
+      {text: 'Home', link: '/'},
+      {text: 'Contact', link: 'https://next.np-overflow.club'}
+    ],
+    sidebar: {
+      '/get-started': [
+        {
+          text: 'Introduction',
+          items: [
+            {text: 'What is this?', link: ''},
+            {text: 'Getting started', link: ''},
+          ]
+        },
+        {
+          text: 'Sessions',
+          items: [
+            {text: 'General Workflow', link: ''},
+            {text: 'Pre-session Admin', link: ''},
+            {text: 'Research', link: ''},
+            {text: 'Slides', link: ''},
+            {text: 'Post-session Wrap-up', link: ''},
+          ]
+        },
+        {
+          text: 'Events',
+          items: [
+            {text: 'CCA Fiesta', link: ''},
+            {text: 'Welcome Party', link: ''},
+            {text: 'ByteHackz', link: ''},
+            {text: 'PIOI', link: ''},
+            {text: 'Bootcamps', link: ''},
+          ]
+        },
+        {
+          text: 'Admin',
+          items: [
+            {text: 'Pre-sem Committee Meet-up', link: ''},
+            {text: 'Interviews', link: ''},
+          ]
+        },
+      ]
+    }
   },
 })
