2bb447aaac3e7ac976816375442e58b08506524e
Qin Guan <qinguan20040914@gmail.com>
2022-08-28 18:00:18 +0800

    feat: dark mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,6 +55,10 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: 'dark',
+        respectPrefersColorScheme: true
+      },
       navbar: {
         title: 'Overflow Handbook',
         logo: {
